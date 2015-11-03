904a70dfb6c18c4f595ea713af38869108203af2
Christophe Gabreau <cgabreau@sfr.fr>
2015-11-03 14:52:26 +0100

    STEER and DRIVE delays changed to 20

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -17,8 +17,8 @@
 
 //using namespace std;
 
-#define STEER_DELAY     15
-#define DRIVE_DELAY     15
+#define STEER_DELAY     20
+#define DRIVE_DELAY     20
 
 int val_steer = 0;         // steer servo value
 int val_drive = 0;         // drive servo value
